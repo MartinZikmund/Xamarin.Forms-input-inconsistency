ed81dc1a5779f00e3d7f4096963cf3ede24fb1cc
Martin Zikmund <martinzikmund@sphereline.com>
2018-05-29 21:06:58 +0200

    Input transparent issue repro

---
+++ InputTransparentIssue/InputTransparentIssue/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace InputTransparentIssue
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    private void TapGestureRecognizer_OnTapped(object sender, EventArgs e)
+	    {
+	        Debug.WriteLine("Tap captured");
+	    }
+	}
+}
